20d8671132ad40aecbf7653936c6dc0fa51a64c8
SKuzovkov <citrusofill@gmail.com>
2023-03-17 14:22:02 +0300

    Решение задачи на языке C#

---
+++ program1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+void PrintIndexAndValues (String[] myArr)  {
+        for(int i = 0; i < myArr.Length; i++)
+        {
+            Console.WriteLine("   [{0}] : {1}", i, myArr[i]);
+        }
+        Console.WriteLine();
+    }
+
+Console.WriteLine ("Введите элементы массива чере пробел");
+string [] arrLetterAll = Console.ReadLine().Split (" ").ToArray();
+string [] arrLetter3 = new string [arrLetterAll.GetLength(0)];
+int j = 0; 
+for (int i=0;i < arrLetterAll.Length;i++)
+{
+if (arrLetterAll[i].Length<3) 
+{
+arrLetter3[j] = arrLetterAll[i];
+j++;
+}
+
+}
+Array.Resize(ref arrLetter3,j);
+
+ PrintIndexAndValues (arrLetter3);
